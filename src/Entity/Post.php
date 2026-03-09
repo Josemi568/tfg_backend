3e2d0354f0f6973e0d25976e71a94f125e63dd72
Jose Miguel Garcia <josegarmal1811@gmail.com>
2026-03-09 12:30:37 +0100

    sixthCommit

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -40,6 +40,9 @@ class Post
     #[ORM\OneToMany(targetEntity: Comment::class, mappedBy: 'post')]
     private Collection $comments;
 
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
     public function __construct()
     {
         $this->comments = new ArrayCollection();
@@ -160,6 +163,18 @@ class Post
                 $comment->setPost(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
 
         return $this;
     }
